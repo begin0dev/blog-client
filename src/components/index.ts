e1be86dd2c9918dd2e01bf146cf86e98abdd7e9f
begin0dev <prosonic1@gmail.com>
2020-07-16 23:28:08 +0900

    TEST: build

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,16 +1,16 @@
-import Auth from './auth';
+import Auth from './Auth';
 
-import Header from './base/header';
+import Header from './base/Header';
 
-import Modal from './common/modal';
-import Overlay from './common/overlay';
-import Spinner from './common/spinner';
-import Progressbar from './common/progressbar';
-import TextInput from './common/textInput';
-import Hamburger from './common/hamburger';
-import HideScrollbar from './common/hideScrollbar';
+import Modal from './common/Modal';
+import Overlay from './common/Overlay';
+import Spinner from './common/Spinner';
+import Progressbar from './common/Progressbar';
+import TextInput from './common/TextInput';
+import Hamburger from './common/Hamburger';
+import HideScrollbar from './common/HideScrollbar';
 
-import PageTemplate from './templates/pageTemplate';
+import PageTemplate from './templates/PageTemplate';
 
 export {
   Auth,
