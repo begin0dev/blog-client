357fe889836d765f7b0ed73a6109c149f34edc1c
begin0dev <prosonic1@gmail.com>
2020-07-16 23:10:42 +0900

    FIX: error

--- src/store/modules/base/actions.ts
+++ src/store/modules/base/actions.ts
@@ -2,10 +2,8 @@ import { createAction } from 'typesafe-actions';
 
 export const SET_IS_MOBILE = 'SET_IS_MOBILE';
 export const SET_LOADING_PERCENT = 'SET_LOADING_PERCENT';
-export const TOGGLE_SIDEBAR = 'TOGGLE_SIDEBAR';
 export const TOGGLE_AUTH_MODAL = 'TOGGLE_AUTH_MODAL';
 
 export const setIsMobile = createAction(SET_IS_MOBILE)<boolean>();
 export const setLoadingPercent = createAction(SET_LOADING_PERCENT)<number>();
-export const toggleSidebar = createAction(TOGGLE_SIDEBAR)<boolean>();
 export const toggleAuthModal = createAction(TOGGLE_AUTH_MODAL)<boolean>();
