c918391f724d70d0e046f7f9156333ebbbc4a78c
begin0dev <prosonic1@gmail.com>
2020-11-03 22:33:50 +0900

    FIX: progressbar

--- src/components/common/Progressbar/Progressbar.tsx
+++ src/components/common/Progressbar/Progressbar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState, useEffect, memo } from 'react';
+import { useState, useEffect, memo, useRef } from 'react';
 
 import { ProgressbarBlock } from './Progressbar.styles';
 
@@ -8,11 +8,13 @@ interface IProps {
 }
 
 function Progressbar({ percent }: IProps) {
+  const timer = useRef<number>();
+
   const [isZero, setIsZero] = useState<boolean>(true);
   const [visible, setVisible] = useState<boolean>(false);
 
   useEffect(() => {
-    let timeOut: number | null = null;
+    if (timer.current) clearTimeout(timer.current);
 
     if (percent === 0) setVisible(true);
     if (percent !== 0) {
@@ -20,22 +22,20 @@ function Progressbar({ percent }: IProps) {
       setVisible(true);
     }
     if (percent !== 0 && percent !== 100) {
-      timeOut = setTimeout(() => {
+      timer.current = setTimeout(() => {
         setIsZero(true);
         setVisible(false);
       }, 3600);
     }
     if (percent === 100) {
-      if (timeOut) clearTimeout(timeOut);
-
-      setTimeout(() => {
+      timer.current = setTimeout(() => {
         setIsZero(true);
         setVisible(false);
-      }, 800);
+      }, 600);
     }
 
     return () => {
-      if (timeOut) clearTimeout(timeOut);
+      if (timer.current) clearTimeout(timer.current);
     };
   }, [percent]);
 
