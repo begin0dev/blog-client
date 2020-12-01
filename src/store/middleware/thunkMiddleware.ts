c67fb65a181916714a1a5d32067fd8dd54ff789a
begin0dev <prosonic1@gmail.com>
2020-12-01 23:17:06 +0900

    CHANGE: saga -> toolkit

--- src/store/middleware/thunkMiddleware.ts
+++ src/store/middleware/thunkMiddleware.ts
@@ -5,7 +5,7 @@ import { actions as baseActions } from 'store/base';
 function thunkMiddleware({ dispatch }: MiddlewareAPI) {
   return (next: Dispatch) => (action: AnyAction) => {
     if (action.type.endsWith('/pending')) dispatch(baseActions.setLoadingPercent(0));
-    if (action.type.endsWith('/fulfilled') || action.type.endsWith('/rejected'))
+    else if (action.type.endsWith('/fulfilled') || action.type.endsWith('/rejected'))
       dispatch(baseActions.setLoadingPercent(100));
     return next(action);
   };
