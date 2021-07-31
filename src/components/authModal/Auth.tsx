7cd243560d7571bfdc90176244dc99f80b26b7cc
begin0dev <prosonic1@gmail.com>
2021-07-31 17:07:23 +0900

    FIX: styles

--- src/components/authModal/Auth.tsx
+++ src/components/authModal/Auth.tsx
@@ -2,30 +2,20 @@ import qs from 'qs';
 import { memo, useCallback, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import styled, { css } from 'styled-components';
 
 import { Modal } from 'components';
 import { ArrowLeft, Facebook, Github, Google, Kakao, Logo } from 'assets/svgs';
-import {
-  AuthBlock,
-  SectionTopBlock,
-  SectionBottomBlock,
-  BackButton,
-  WelcomeBlock,
-  LogoWrapper,
-  StartHeader,
-  SocialDescBlock,
-  SocialBlock,
-  SocialButton,
-} from './Auth.styles';
 import { RootState } from '../../stores';
-import useToasts from '../common/toast/useToasts';
-import useCheckBreakPoint from '../../lib/hooks/useCheckBreakPoint';
-import { breakPoints } from '../../styles/utils';
+import { breakPoints, includeMedia } from '../../styles/utils';
 import { actions as baseActions } from '../../stores/base';
 import { baseURL } from '../../lib/services/apiClient';
 import { V1_SOCIALS_URL } from '../../lib/services/auth';
 import { actions as userActions } from '../../stores/user';
 import { palette } from '../../styles/palette';
+import { pulseKeyframes } from '../../styles/baseCss';
+import useToasts from '../common/toast/useToasts';
+import useCheckBreakPoint from '../../lib/hooks/useCheckBreakPoint';
 
 function Auth() {
   const history = useHistory();
@@ -116,3 +106,130 @@ function Auth() {
 }
 
 export default memo(Auth);
+
+const flexCss = css`
+  display: flex;
+  flex: 1;
+`;
+
+const AuthBlock = styled.div`
+  ${flexCss};
+  position: relative;
+  flex-flow: column wrap;
+  justify-content: space-between;
+  padding: 75px 35px;
+  overflow-x: hidden;
+  overflow-y: auto;
+  ${includeMedia('<=sm')} {
+    height: 100%;
+  }
+  ${includeMedia('>sm')} {
+    min-height: 640px;
+  }
+`;
+const SectionTopBlock = styled.div`
+  ${flexCss};
+  flex-flow: column wrap;
+`;
+const SectionBottomBlock = styled(SectionTopBlock)`
+  justify-content: flex-end;
+`;
+const BackButton = styled.button`
+  width: 30px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent;
+  border-radius: 100%;
+  margin-bottom: 55px;
+  margin-left: -3px;
+  svg {
+    color: ${palette.gray6};
+    font-size: 26px;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`;
+const WelcomeBlock = styled.div`
+  ${flexCss};
+  flex-flow: column wrap;
+  font-size: 20px;
+  font-weight: 400;
+  color: #333;
+  letter-spacing: -0.01em;
+  div + div {
+    margin-top: 5px;
+  }
+`;
+const LogoWrapper = styled.span`
+  line-height: 0;
+  margin-right: 7px;
+  svg {
+    height: 30px;
+    margin-top: -5px;
+    margin-bottom: -3px;
+  }
+`;
+const StartHeader = styled.h2`
+  font-size: 18px;
+  margin-bottom: 3px;
+`;
+const SocialDescBlock = styled.div`
+  font-size: 13px;
+  color: ${palette.gray7};
+  margin-bottom: 20px;
+`;
+const SocialBlock = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  button + button {
+    margin-left: 20px;
+  }
+`;
+const SocialButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 42px;
+  height: 42px;
+  border-radius: 100%;
+  overflow: hidden;
+  &.facebook {
+    background-color: #4267b2;
+    color: #ffffff;
+    svg {
+      font-size: 20px;
+    }
+  }
+  &.google {
+    background-color: #ea4334;
+    color: #ffffff;
+    svg {
+      font-size: 20px;
+    }
+  }
+  &.kakao {
+    background-color: #f7e600;
+    color: #3c1e1e;
+    svg {
+      font-size: 28px;
+    }
+  }
+  &.github {
+    background-color: transparent;
+    color: ${palette.gray9};
+    svg {
+      font-size: 42px;
+    }
+  }
+  &:active {
+    animation: ${pulseKeyframes} 0.3s;
+  }
+  &:hover,
+  &:focus,
+  &:active {
+    opacity: 0.8;
+  }
+`;
--- src/components/common/hamburger/Hamburger.tsx
+++ src/components/common/hamburger/Hamburger.tsx
@@ -1,6 +1,6 @@
 import { memo } from 'react';
+import styled, { css } from 'styled-components';
 
-import { HamburgerWrapper } from './Hamburger.styles';
 import { palette } from '../../../styles/palette';
 
 interface IProps {
@@ -37,3 +37,74 @@ function Hamburger({
 }
 
 export default memo(Hamburger);
+
+const HamburgerWrapper = styled.div<{
+  width: string;
+  height: string;
+  spacing: string;
+  color: string;
+  active: boolean;
+}>`
+  // Normalize
+  color: inherit;
+  text-transform: none;
+  background-color: transparent;
+  border: 0;
+  margin: 0;
+  overflow: visible;
+  cursor: pointer;
+
+  transition-property: opacity, filter;
+  transition-duration: 0.15s;
+  transition-timing-function: linear;
+
+  ${({ width, height, spacing, color, active }) => css`
+    & > span.box {
+      position: relative;
+      display: block;
+      width: ${width};
+      height: calc(${height} * 3 + ${spacing} * 2);
+    }
+    & > span.box > span.line {
+      display: block;
+      top: calc(${height} / 2);
+      margin-top: calc(${height} / -2);
+      &,
+      &:before,
+      &:after {
+        position: absolute;
+        width: ${width};
+        height: ${height};
+        border-radius: calc(${height} / 2);
+        background-color: ${color};
+        transition-property: transform, opacity;
+        transition-duration: 0.15s;
+        transition-timing-function: ease;
+      }
+      &:before,
+      &:after {
+        content: '';
+        display: block;
+      }
+      &:before {
+        top: calc(${height} + ${spacing});
+      }
+      &:after {
+        top: calc(${height} * 2 + ${spacing} * 2);
+      }
+    }
+    ${active &&
+    css`
+      & > span.box > span.line {
+        transform: translate3d(0, calc(${spacing} + ${height}), 0) rotate(-45deg);
+        &:before {
+          transform: rotate(45deg) translate3d(calc(${width} / 7), calc(${spacing} * -1), 0);
+          opacity: 0;
+        }
+        &:after {
+          transform: translate3d(0, calc((${spacing} + ${height}) * -2), 0) rotate(90deg);
+        }
+      }
+    `}
+  `}
+`;
