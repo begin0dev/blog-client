f827664774c6106d05ed7cd39d43c19e2497f8dc
begin0dev <prosonic1@gmail.com>
2022-01-22 11:41:59 +0900

    FIX: lazy load hooks

--- src/hooks/useLazyImageObserver.ts
+++ src/hooks/useLazyImageObserver.ts
@@ -1,20 +1,28 @@
 import { useEffect, useRef } from 'react';
 
 function useLazyImageObserver(options?: IntersectionObserverInit) {
-  const imageRef = useRef<HTMLImageElement | null>(null);
+  const imageRef = useRef<HTMLImageElement>();
   const intersectionOptions = useRef<IntersectionObserverInit | undefined>(options);
 
   useEffect(() => {
     if (!imageRef.current) return;
+
     const io = new IntersectionObserver(([entry], observe) => {
       if (!entry.isIntersecting) return;
       const target = entry.target as HTMLImageElement;
-      target.src = target.dataset.src as string;
+      const datasetSrc = target.dataset.src;
+      if (datasetSrc) target.src = datasetSrc;
       observe.unobserve(target);
       observe.disconnect();
     }, intersectionOptions.current);
+
     io.observe(imageRef.current);
+    return () => {
+      io.disconnect();
+    };
   }, []);
+
+  return { imageRef };
 }
 
 export default useLazyImageObserver;
