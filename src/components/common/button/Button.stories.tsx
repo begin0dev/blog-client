ed8213567344aab6b322c872879db19fa619e05a
begin0dev <prosonic1@gmail.com>
2022-01-31 16:36:36 +0900

    ADD: hamburger storybook

--- src/components/common/button/Button.stories.tsx
+++ src/components/common/button/Button.stories.tsx
@@ -1,18 +1,17 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import { IcSearch } from 'assets/svgs';
-
 import Button from './Button';
 
 export default {
-  component: Button,
   title: Button.name,
+  component: Button,
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const BasicButton = Template.bind({});
-BasicButton.args = {
+export const TextButton = Template.bind({});
+TextButton.args = {
   children: 'Button',
 };
 
