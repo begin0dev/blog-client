6fe085280e78dd47b661235c2b96a1e4459c0a3c
begin0dev <prosonic1@gmail.com>
2022-03-02 21:35:35 +0900

    FIX: css

--- src/components/common/searchInput/SearchInput.tsx
+++ src/components/common/searchInput/SearchInput.tsx
@@ -9,51 +9,49 @@ interface Props extends InputHTMLAttributes<any> {
   fontSize?: string;
 }
 
-const primaryColor = themes.PRIMARY;
-const borderColor = palette.GRAY1;
-
 function SearchInput({ fontSize = '12px', ...restProps }: Props) {
   return (
-    <SearchInputBlock fontSize={fontSize} primaryColor={primaryColor}>
+    <SearchInputBlock fontSize={fontSize}>
       <IcSearch />
-      <Input {...restProps} />
+      <input {...restProps} />
     </SearchInputBlock>
   );
 }
 
 export default SearchInput;
 
-const SearchInputBlock = styled.div<{ fontSize: string; primaryColor: string }>`
+const primaryColor = themes.PRIMARY;
+const borderColor = palette.GRAY1;
+
+const SearchInputBlock = styled.div<{ fontSize: string }>`
   position: relative;
   font-size: ${({ fontSize }) => fontSize};
   color: inherit;
-
+  transition: opacity 0.2s ease-in-out;
+  opacity: 0.8;
   > svg {
     position: absolute;
     top: 50%;
     left: 16px;
+    font-size: 1.3em;
     color: ${primaryColor};
     transform: translateY(-50%);
-    font-size: 1.3em;
-    opacity: 0.8;
   }
-
-  &:focus-within {
-    > svg {
-      opacity: 1;
-    }
+  &:focus-within,
+  &:hover {
+    opacity: 1;
   }
-`;
-const Input = styled.input<{ borderColor?: string }>`
-  font-size: inherit;
-  width: 100%;
-  color: inherit;
-  border: 1px solid ${borderColor};
-  border-radius: 2em;
-  padding: 8px 18px 8px 36px;
-  background-color: transparent;
-  &::placeholder {
+  > input {
     font-size: inherit;
-    opacity: 0.4;
+    width: 100%;
+    color: inherit;
+    border: 1px solid ${borderColor};
+    border-radius: 2em;
+    padding: 8px 18px 8px 36px;
+    background-color: transparent;
+    &::placeholder {
+      font-size: inherit;
+      opacity: 0.6;
+    }
   }
 `;
