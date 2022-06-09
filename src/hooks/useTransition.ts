bce0ebf6e708efcecd1893967bce9f7ce6521733
begin0dev <prosonic1@gmail.com>
2022-06-10 00:17:21 +0900

    FIX: unmount

--- src/hooks/useTransition.ts
+++ src/hooks/useTransition.ts
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 
 import { ValueOf } from 'lib/utils/typescript-utils';
+import { useUnMount } from './useUnMount';
 
 export const transitionStatus = {
   ENTERING: 'entering',
@@ -37,11 +38,11 @@ export function useTransition({ active, duration = 200 }: Props) {
     timer.current = setTimeout(() => {
       setStatus((prevState) => getNextStatus(prevState));
     }, duration);
-
-    return () => {
-      if (timer.current) clearTimeout(timer.current);
-    };
   }, [active, duration, status]);
+
+  useUnMount(() => {
+    if (timer.current) clearTimeout(timer.current);
+  });
 
   return status;
 }
