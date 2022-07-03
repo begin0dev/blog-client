e4a7373ff4500f57d10244e773f0ab9ef6daa81a
begin0dev <prosonic1@gmail.com>
2022-07-03 13:54:29 +0900

    REMOVE: main

--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,23 +1,5 @@
-import { useToast } from '../../components/common';
-
 function MainPage() {
-  const { addToast } = useToast();
-
-  return (
-    <div>
-      main
-      <button
-        type="button"
-        onClick={() =>
-          addToast({
-            message: 'test asfsadfasdfasdf dsfasd fasdf asfdsadf asd',
-          })
-        }
-      >
-        add
-      </button>
-    </div>
-  );
+  return <div>main</div>;
 }
 
 export default MainPage;
