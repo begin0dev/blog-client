5390474c42106826368952bf330d936bd773eb40
begin0dev <prosonic1@gmail.com>
2022-10-12 22:34:33 +0900

    CHANGE: stitches

---
+++ src/components/common/spinner/spinner.styles.ts
@@ -0,0 +1,28 @@
+import { styled, keyframes } from '@stitches/react';
+
+const circleFadeDelayKeyframes = keyframes({
+  '0%': { opacity: 1 },
+  '50%, 100%': { opacity: 0 },
+});
+
+export const SpinnerWrapper = styled('div', {
+  position: 'relative',
+});
+
+export const CircleWrapper = styled('span', {
+  position: 'absolute',
+  left: 0,
+  top: 0,
+  width: '100%',
+  height: '100%',
+
+  '&::before': {
+    content: '',
+    display: 'block',
+    margin: '0 auto',
+    width: '15%',
+    height: '15%',
+    borderRadius: '100%',
+    animation: `${circleFadeDelayKeyframes} 1.2s infinite ease-in-out both`,
+  },
+});
